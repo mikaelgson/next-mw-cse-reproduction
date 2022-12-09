0ccbbc41e9cf740ed906a0b74124dc49ea7767b5
Mikael Gustafsson <mikael.gustafsson@adlibris.com>
2022-12-09 13:46:20 +0100

    remove i18n

--- next.config.js
+++ next.config.js
@@ -7,22 +7,6 @@ module.exports = {
 
   reactStrictMode: true,
 
-  i18n: {
-    locales: ["default", "sv", "sv-FI", "nb", "fi"],
-    defaultLocale: "default",
-  },
-
-  images: {
-    // https://nextjs.org/docs/api-reference/next/image#domains
-    domains: ["images.ctfassets.net"],
-
-    // https://nextjs.org/docs/api-reference/next/image#device-sizes
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-
-    // https://nextjs.org/docs/api-reference/next/image#device-sizes
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-
   async rewrites() {
     return [
       {
